bf2a4fe5b865540548be9c18e487e67e540d3435
chef-pepe <ap334@disroot.org>
2020-10-17 16:26:49 -0700

    Finish integration tests

--- test/claimAndMint.js
+++ test/claimAndMint.js
@@ -44,18 +44,72 @@ describe("claimAndMint", function() {
   });
 
   it("should increment token ID for calls with a new sig", async function() {
-    // TODO... do repetition
+    const Minter = await ethers.getContractFactory("Minter");
+    const PepeV2 = await ethers.getContractFactory("PepeV2");
 
+    const minter = await Minter.deploy(testMerkleTree.root);
+    const pepeV2 = await PepeV2.deploy(minter.address, testSigs, testIpfsHashes);
+    await minter.setMintable(pepeV2.address);
+
+    const leaf = testMerkleTree.leaves[0];
+    const vals = testMerkleTree.leafToVals[leaf];
+    const branch = testMerkleTree.leafToBranch[leaf];
+
+    await minter.claim(vals[0], vals[1], vals[2], vals[3], branch)
+
+    const leaf2 = testMerkleTree.leaves[2];
+    const vals2 = testMerkleTree.leafToVals[leaf2];
+    const branch2 = testMerkleTree.leafToBranch[leaf2];
+
+    await minter.claim(vals2[0], vals2[1], vals2[2], vals2[3], branch2)
+
+    const balance = await pepeV2.balanceOf(vals2[2], 2);
+    expect(balance).to.equal(vals2[3]);
+    const emptyBalance = await pepeV2.balanceOf(vals2[2], 1);
+    expect(emptyBalance).to.equal(0);
   });
-
-
-  it("should fail if index has already been claimed", async function() {
-
-  });
-
-
 
   it("use the same token ID for subsequent calls with the same sig", async function() {
+    const Minter = await ethers.getContractFactory("Minter");
+    const PepeV2 = await ethers.getContractFactory("PepeV2");
 
+    const minter = await Minter.deploy(testMerkleTree.root);
+    const pepeV2 = await PepeV2.deploy(minter.address, testSigs, testIpfsHashes);
+    await minter.setMintable(pepeV2.address);
+
+    const leaf = testMerkleTree.leaves[0];
+    const vals = testMerkleTree.leafToVals[leaf];
+    const branch = testMerkleTree.leafToBranch[leaf];
+
+    await minter.claim(vals[0], vals[1], vals[2], vals[3], branch)
+
+    const leaf2 = testMerkleTree.leaves[1];
+    const vals2 = testMerkleTree.leafToVals[leaf2];
+    const branch2 = testMerkleTree.leafToBranch[leaf2];
+
+    await minter.claim(vals2[0], vals2[1], vals2[2], vals2[3], branch2)
+
+    const balance = await pepeV2.balanceOf(vals2[2], 1);
+    expect(balance).to.equal(vals2[3]);
+    const emptyBalance = await pepeV2.balanceOf(vals2[2], 2);
+    expect(emptyBalance).to.equal(0);
+  });
+
+  it("should fail if index has already been claimed", async function() {
+    const Minter = await ethers.getContractFactory("Minter");
+    const PepeV2 = await ethers.getContractFactory("PepeV2");
+
+    const minter = await Minter.deploy(testMerkleTree.root);
+    const pepeV2 = await PepeV2.deploy(minter.address, testSigs, testIpfsHashes);
+    await minter.setMintable(pepeV2.address);
+
+    const leaf = testMerkleTree.leaves[0];
+    const vals = testMerkleTree.leafToVals[leaf];
+    const branch = testMerkleTree.leafToBranch[leaf];
+
+    await minter.claim(vals[0], vals[1], vals[2], vals[3], branch)
+
+    await expect(minter.claim(vals[0], vals[1], vals[2], vals[3], branch))
+      .to.be.revertedWith("Minter: Can't claim a drop that's already been claimed");
   });
 });
